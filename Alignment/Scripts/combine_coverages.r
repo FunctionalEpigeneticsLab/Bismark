644f44937a588e9b81313014bc297b3083aca61f
KobeDR <kobederidder@MacBook-Pro-van-Kobe.local>
2023-10-27 14:41:37 +0200

    .

--- Alignment/Scripts/combine_coverages.r
+++ Alignment/Scripts/combine_coverages.r
@@ -1,4 +1,4 @@
 args = commandArgs(trailingOnly=TRUE)
 f <- args[1]
 df = read.delim(f, header = F)
-cat(paste0(paste(f, mean(as.numeric(df$V5)), sep = '\t'), '\n'), file = 'on_target.coverages', append = TRUE)
+cat(paste0(paste(f, mean(as.numeric(df[, ncol(df)])), sep = '\t'), '\n'), file = 'on_target.coverages', append = TRUE)
