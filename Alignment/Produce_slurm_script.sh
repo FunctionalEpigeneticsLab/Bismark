1a4afaaf44f9b1f9b272a633b0a48ac32b0ab6f6
KobeDR <kobederidder@macbookpro.home>
2025-02-20 20:07:38 +0100

    .

--- Alignment/Produce_slurm_script.sh
+++ Alignment/Produce_slurm_script.sh
@@ -10,4 +10,4 @@ echo -e "#!/bin/bash
 #SBATCH --cluster wice --account lcfdna_labepi
 
 cd $(pwd)
-/lustre1/scratch/343/vsc34344/micromamba/bin/nextflow run main.nf -resume -params-file params_file.yml" > nextflow_submitter.slurm
+/vsc-hard-mounts/leuven-data/343/vsc34344/micromamba/envs/Rbase/bin/nextflow run main.nf -resume -params-file params_file.yml" > nextflow_submitter.slurm
